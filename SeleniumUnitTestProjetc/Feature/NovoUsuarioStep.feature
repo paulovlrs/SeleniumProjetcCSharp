00f8a6e6a8e70b56b8db1aee33b6515496eb3077
paulovlrs <paulovlrs@outlook.com>
2020-03-09 17:02:49 -0300

    Adicionado Specflow no projeto, criado funções para verificar sucesso ou falha ao criar novo usuário

---
+++ SeleniumUnitTestProjetc/Feature/NovoUsuarioStep.feature
@@ -0,0 +1,14 @@
+﻿Feature: NovoUsuarioStep
+	Verificar se o cadastro de usuário está sendo inserido com sucesso
+
+@cadastro
+Scenario: Adicionar Usuario
+	Given Que eu esteja na tela principal
+	And Acesso o cadastro de usuario
+	When Prencher os dados de entrada
+	| Nome  | Ultimo Nome | Email           | Endereco | Universidade | Profissao   | Genero    | Idade |
+	| Paulo | Silva       | teste@teste.com | Rua 1    | PUC          | QA Engineer | Masculino | 29    |
+	And Salvar
+	Then O Sistema retorna mensagem
+	| Mensagem                   |
+	| Usuário Criado com sucesso |
--- SeleniumUnitTestProjetc/Pages/NovoUsuarioPage.cs
+++ SeleniumUnitTestProjetc/Pages/NovoUsuarioPage.cs
@@ -2,60 +2,89 @@
 using SeleniumExtras.PageObjects;
 using SeleniumBasicProjectConfiguration.Base;
 using SeleniumBasicProjectConfiguration.Extensions;
+using System;
+using System.Collections.Generic;
 
 namespace SeleniumUnitTestProjetc.Pages
 {
     public class NovoUsuarioPage : BasePage
     {
         [FindsBy(How = How.Id, Using = "user_name")]
-        private IWebElement InputNome { get; set; }
+        private IWebElement InputUser_name { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_lastname")]
-        private IWebElement InputUltimoNome { get; set; }
+        private IWebElement InputUser_lastname { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_email")]
-        private IWebElement InputEmail { get; set; }
+        private IWebElement InputUser_email { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_address")]
-        private IWebElement InputEndereco { get; set; }
+        private IWebElement InputUser_address { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_university")]
-        private IWebElement InputUniversidade { get; set; }
+        private IWebElement InputUser_university { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_profile")]
-        private IWebElement InputProfissao { get; set; }
+        private IWebElement InputUser_profile { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_gender")]
-        private IWebElement InputGenero { get; set; }
+        private IWebElement InputUser_gender { get; set; }
 
         [FindsBy(How = How.Id, Using = "user_age")]
-        private IWebElement InputIdade { get; set; }
+        private IWebElement InputUser_age { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "[class = 'actions btn waves-effect green']")]
-        private IWebElement ButtonCriar { get; set; }
+        private IWebElement ButtonCreate { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "[class = 'btn waves-light red']")]
+        private IWebElement ButtonBack { get; set; }
+
+        [FindsBy(How = How.Id, Using = "error_explanation")]
+        private IList<IWebElement> MessageErro { get; set; }
+
+        [FindsBy(How = How.Id, Using = "notice")]
+        private IList<IWebElement> MessageNotice { get; set; }
 
         internal void CheckIfNovoUsuarioExist()
         {
-            InputNome.AssertElementPresent();
-        }
+            InputUser_name.AssertElementPresent();
+        }        
 
-        [FindsBy(How = How.CssSelector, Using = "[class = 'btn waves-light red']")]
-        private IWebElement ButtonVoltar { get; set; }
+        public void MensagemAlerta(string mensagem)
+        {
+            //IList<IWebElement> all = DriverContext.Driver.FindElements(By.Id("error_explanation"));
+ 
+            String[] allText = new String[MessageErro.Count];
+            int i = 0;
+            
+            // Criar validação da mensagem de erro esperado
+            foreach (IWebElement element in MessageErro)
+            {
+                allText[i++] = element.Text;
+            }         
+        }
 
         /// <summary>
         /// Preenche os campos da tela de "Cadastrar usuário".
         /// </summary>
         public void PreencherCampos(string nome, string ultimoNome, string email, string endereco, string universidade, string profissao, string genero, int idade)
         {
-            InputNome.SendKeys(nome);
-            InputUltimoNome.SendKeys(ultimoNome);
-            InputEmail.SendKeys(email);
-            InputEndereco.SendKeys(endereco);
-            InputUniversidade.SendKeys(universidade);
-            InputProfissao.SendKeys(profissao);
-            InputGenero.SendKeys(genero);
-            InputIdade.SendKeys(idade.ToString());
-            ButtonCriar.Click();
+            InputUser_name.SendKeys(nome);
+            InputUser_lastname.SendKeys(ultimoNome);
+            InputUser_email.SendKeys(email);
+            InputUser_address.SendKeys(endereco);
+            InputUser_university.SendKeys(universidade);
+            InputUser_profile.SendKeys(profissao);
+            InputUser_gender.SendKeys(genero);
+            InputUser_age.SendKeys(idade.ToString());
+            ButtonCreate.Click();
+        }
+
+        public string MensagemSucesso(string mensagem)
+        {
+            // criar validação de sucesso
+
+            return null;
         }
     }
 }
